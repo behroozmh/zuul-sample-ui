592e00f1e6737c6e5657af7c34fed417093a8ef4
behrooz.mhoammadzadeh <brhooz.mh@gmail.com>
2021-04-18 12:35:35 +0430

    behrooz.mh: add CustomLogoutHandler logoutHandler

--- src/main/java/ir/pt/struct/config/WebSecurityConfigJS.java
+++ src/main/java/ir/pt/struct/config/WebSecurityConfigJS.java
@@ -1,5 +1,7 @@
 //package ir.pt.struct.config;
 //
+//import ir.pt.struct.handler.CustomLogoutHandler;
+//import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +13,9 @@
 //@EnableOAuth2Sso
 //public class WebSecurityConfigJS extends WebSecurityConfigurerAdapter {
 //
+//    @Autowired
+//    private CustomLogoutHandler logoutHandler;
+//
 //    @Override
 //    public void configure(HttpSecurity http) throws Exception {
 //        http
@@ -20,6 +25,8 @@
 //                "/bower_components/**", "/resources/**", "/logout").permitAll()
 //                .anyRequest().authenticated()
 //                .and().csrf().ignoringAntMatchers("/logout")
-//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
+//                .logout().addLogoutHandler(logoutHandler);
 //    }
 //}
