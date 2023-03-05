a1a484b4a9ba216ee89ef1ec683446620d111358
behrooz.mh <behrooz.mh@gmail.com>
2023-03-05 18:04:54 +0330

    behrooz.mh: change CORS filter for Cors add to header

--- src/main/java/ir/pt/struct/filter/CorsFilter.java
+++ src/main/java/ir/pt/struct/filter/CorsFilter.java
@@ -7,23 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpHeaders;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Set;
 
 @Configuration("myCorsFilter")
-@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
     private Logger log = LoggerFactory.getLogger(this.getClass());
-
-    @Autowired
     private Dashboard dashboard;
+
+    public CorsFilter(Dashboard dashboard) {
+        this.dashboard = dashboard;
+    }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
@@ -31,14 +30,12 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         //
         String origins = dashboard.getCORS_ALLOW_ORIGINS();
-        String hosts = dashboard.getALLOW_HOST();
         String methods = dashboard.getCORS_ALLOW_METHODS();
         //
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,x-www-form-urlencoded,access-control-allow-origin, authorization, content-type, x-requested-with");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", methods);
-
         String reqOrigin = request.getHeader("origin");
         String defaultOrigin = origins.split(",")[0];
 
@@ -49,16 +46,6 @@ public class CorsFilter implements Filter {
             response.setHeader("Access-Control-Allow-Origin", reqOrigin);
         else {
             response.setHeader("Access-Control-Allow-Origin", defaultOrigin);
-        }
-
-        ///TODO://BEHI007 remove to other Filter for check referer
-        String host = request.getHeader("host");
-        String reqReferer = request.getHeader("Referer");
-        String reqUrl = request.getRequestURI();
-        log.info("######## host= " + host + " ## Referer= " + reqReferer
-                + " ## getRequestURI=" + reqUrl + " ########");
-        if (!matchHostsWhiteList(host, hosts)) {
-            throw new ServletException("Host failed: Host is not valid");
         }
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
@@ -82,21 +69,8 @@ public class CorsFilter implements Filter {
         return false;
     }
 
-    private boolean matchHostsWhiteList(String reqValue, String whiteList) {
-        if (reqValue == null || reqValue.isEmpty() || "".equalsIgnoreCase(reqValue)
-                || whiteList.equalsIgnoreCase("*"))
-            return true;
-        whiteList = StringUtils.trimAllWhitespace(whiteList);
-        Set<String> whiteListOrigin = StringUtils.commaDelimitedListToSet(whiteList);
-        if (whiteListOrigin != null && whiteListOrigin.size() > 0) {
-            return whiteListOrigin.stream().anyMatch(f -> f.equalsIgnoreCase(reqValue));
-        }
-        return false;
-    }
-
     @Override
     public void init(FilterConfig filterConfig) {
-
     }
 
     @Override
